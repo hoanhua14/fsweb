dc466ecabdc8021e4d247e0861d4c32a6dc03bfd
hoanhua14 <huasohoan@gmail.com>
2024-07-16 22:25:55 -0700

    customized logging middleware

--- 3.4 Middleware/index2.js
+++ 3.4 Middleware/index2.js
@@ -1,8 +1,13 @@
 import express from "express";
-
+import morgan from "morgan";
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+  console.log("req type", req.method);
+  next();
+});
+app.use(morgan("tiny"));
 app.get("/", (req, res) => {
   res.send("Hello");
 });
