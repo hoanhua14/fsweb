f36b2e65ed40d8d1f557c7840af1bb7eaf13fd8e
hoanhua14 <huasohoan@gmail.com>
2024-07-16 23:58:22 -0700

    customized logging middleware and finished generator

--- 3.4 Middleware/index4.js
+++ 3.4 Middleware/index4.js
@@ -1,8 +1,33 @@
 import express from "express";
-
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+const logger = (req, res, next) => {
+  console.log("Req type", req.method);
+  console.log("Req url", req.url);
+  next();
+};
+const nameGenerator = (req, res, next) => {
+  if (req.method == "POST") {
+    next();
+  }
+};
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(logger);
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  const bandName = `Band name is ${req.body.street} ${req.body.pet}`;
+  res.send(bandName);
 });
